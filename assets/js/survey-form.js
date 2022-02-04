253ac60f4702cffee6dce4aa92c932d436e8a478
Chris Grigg <chris@judgeit.com.au>
2022-02-05 09:51:18 +1000

    Remove Typeform and add checkout page and form to wizard

--- assets/js/survey-form.js
+++ assets/js/survey-form.js
@@ -1,50 +1,100 @@
-// var form = document.getElementById("form");
-// form.addEventListener("submit", formSubmit);
-
-// function formSubmit(e) {
-//     e.preventDefault()
-
-//     const formData = new FormData();
-//     formData.append(
-//         'name',
-//         document.querySelector('input[name="name"]').value
-//     )
-//     formData.append(
-//         'email',
-//         document.querySelector('input[name="email"]').value
-//     )
-
-//     fetch("https://getform.io/f/115e7883-4e74-42b6-9f18-0591443cb828",
-//     {
-//         method: "POST",
-//         body: formData,
-//     })
-//     .then(response => console.log(response))
-//     .catch(error => console.log(error))
-// }
-
 (function () {
   "use strict";
-  window.addEventListener(
-    "load",
-    function () {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName("needs-validation");
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function (form) {
-        form.addEventListener(
-          "submit",
-          function (event) {
-            if (form.checkValidity() === false) {
-              event.preventDefault();
-              event.stopPropagation();
-            }
-            form.classList.add("was-validated");
-          },
-          false
-        );
-      });
+
+  let price = 0;
+
+  // *** form to wizard *** //
+  var $signupForm = $("#SignupForm");
+
+  $signupForm.validate({
+    errorElement: "em",
+    submitHandler: function () {
+      showSpinner();
+      const formData = new FormData();
+      //   formData.append(
+      //     "wasteType",
+      //     document.querySelector('select[name="wasteType"]').value
+      //   );
+      formData.append(
+        "skipDate",
+        document.querySelector('input[name="skipDate"]').value
+      );
+
+      fetch("https://getform.io/f/115e7883-4e74-42b6-9f18-0591443cb828", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          console.log(response);
+          hideSpinner();
+          if (
+            typeof window !== "undefined" &&
+            typeof window.checkoutPage !== "undefined"
+          ) {
+            window.checkoutPage.open({
+              seller: "skipty-skip-hire",
+              checkout: "Four-yard",
+              query: { testing: "1" },
+            });
+          }
+          // form.submit();
+        })
+        .catch((error) => console.error(error));
     },
-    false
-  );
+  });
+
+  $signupForm.formToWizard({
+    submitButton: "saveAccount",
+    nextBtnClass: "btn btn-primary next",
+    prevBtnClass: "btn btn-default prev",
+    buttonTag: "button",
+    validateBeforeNext: function (form, step) {
+      var stepIsValid = true;
+      var validator = form.validate();
+      $(":input", step).each(function (index) {
+        var xy = validator.element(this);
+        stepIsValid = stepIsValid && (typeof xy == "undefined" || xy);
+      });
+      return stepIsValid;
+    },
+    progress: function (i, count) {
+      $("#progress-complete").width("" + (i / count) * 100 + "%");
+    },
+  });
+
+  $('input[name="skipDate"]').daterangepicker({
+    locale: {
+      format: "DD/MM/YY",
+    },
+  });
+
+  $("#4yard").bind("click", function (e) {
+    price = 1;
+    $("#totalPrice").text(`£${price}`);
+  });
+
+  $("#6yard").bind("click", function (e) {
+    price = 1;
+    $("#totalPrice").text(`£${price}`);
+  });
+
+  $("#8yard").bind("click", function (e) {
+    price = 1;
+    $("#totalPrice").text(`£${price}`);
+  });
+
+  $("#12yard").bind("click", function (e) {
+    price = 1;
+    $("#totalPrice").text(`£${price}`);
+  });
+
+  function hideSpinner() {
+    document.getElementById("spinner").style.display = "none";
+    document.getElementById("saveAccount").style.display = "block";
+  }
+  function showSpinner() {
+    document.getElementById("saveAccount").style.display = "none";
+    document.getElementById("spinner").style.display = "block";
+  }
+  hideSpinner();
 })();
